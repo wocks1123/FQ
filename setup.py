480d7ef9806650262e54381b6b437e0ee51ce142
wocks1123 <wocks1123@gmail.com>
2021-10-22 01:49:19 +0900

    requirements 수정

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
     package_dir={'': 'src'},
     python_requires='>=3.6',
     install_requires=[
-        'click==8.0.3',
-        'numpy==1.21.2',
-        'opencv-python==4.5.3.56',
-        'python-dotenv==0.19.1',
-        'pyzmq==22.3.0',
-        'zmq==0.0.0'
+        'click',
+        'numpy',
+        'opencv-python',
+        'python-dotenv',
+        'pyzmq',
+        'zmq'
     ],
     entry_points={
         'console_scripts': [
